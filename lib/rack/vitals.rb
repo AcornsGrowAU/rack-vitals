f33b7fea2ccd2223932f3a53ea9b51fc26ef9172
Ryan Hedges <ryanhedges@gmail.com>
2015-11-25 14:51:45 -0800

    Initial commit

---
+++ lib/rack/vitals.rb
@@ -0,0 +1,7 @@
+require "rack/vitals/version"
+
+module Rack
+  module Vitals
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/vitals/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Vitals
+    VERSION = "0.1.0"
+  end
+end
