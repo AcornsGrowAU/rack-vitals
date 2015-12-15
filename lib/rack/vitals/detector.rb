1252cf4f38a3c7ceff2f489a99eff550d11074a2
Ryan Hedges <ryanhedges@gmail.com>
2015-12-15 14:59:47 -0800

    Move away from compacted class definition
    I did this because I believe that this is introducing loading problems for
    ruby.

--- lib/rack/vitals/detector.rb
+++ lib/rack/vitals/detector.rb
@@ -1,27 +1,29 @@
 module Rack
-  class Vitals::Detector
-    def initialize(registrar)
-      @registrar = registrar
-    end
-
-    def critical_checks_healthy?
-      @registrar.critical_checks.each do |check|
-        check_result = ::Rack::Vitals::CheckEvaluator.run(check)
-        return false if check_result.down?
+  class Vitals
+    class Detector
+      def initialize(registrar)
+        @registrar = registrar
       end
-      return true
-    end
 
-    def generate_status_response
-      response_body = @registrar.all_checks.map do |check|
-        check_result = ::Rack::Vitals::CheckEvaluator.run(check)
-        format_check_to_response_body(check_result)
+      def critical_checks_healthy?
+        @registrar.critical_checks.each do |check|
+          check_result = ::Rack::Vitals::CheckEvaluator.run(check)
+          return false if check_result.down?
+        end
+        return true
       end
-      return response_body.to_json
-    end
 
-    def format_check_to_response_body(check_result)
-      return { name: check_result.name, state: check_result.state }
+      def generate_status_response
+        response_body = @registrar.all_checks.map do |check|
+          check_result = ::Rack::Vitals::CheckEvaluator.run(check)
+          format_check_to_response_body(check_result)
+        end
+        return response_body.to_json
+      end
+
+      def format_check_to_response_body(check_result)
+        return { name: check_result.name, state: check_result.state }
+      end
     end
   end
 end
